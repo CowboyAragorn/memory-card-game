2185f7063a43ee19bc3d09d80fa40b38eca79ec7
Jacob Potter <94807302+CowboyAragorn@users.noreply.github.com>
2022-11-08 14:06:04 -0500

    feat: create score component that displays current and best achieved score

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
+import Score from "./Score";
+import originalCards from "./originalCards";
 
 function Cards() {
-  const [cards, setCards] = useState([
-    { name: 0, selected: false },
-    { name: 1, selected: false },
-    { name: 2, selected: false },
-    { name: 3, selected: false },
-    { name: 4, selected: false },
-  ]);
+  //set cards state equal to imported original cards
+  const [cards, setCards] = useState(originalCards);
 
   useEffect(() => {
     console.log("component did mount");
+  }, [cards]);
+
+  //Randomize order of the cards for game
+  const randomizeCards = () => {
     let currentIndex = cards.length;
     let cardsCopy = [...cards];
     while (currentIndex !== 0) {
@@ -22,16 +23,45 @@ function Cards() {
         cardsCopy[currentIndex],
       ];
     }
-    console.log(cardsCopy);
     setCards(cardsCopy);
-    console.log(cards);
-  }, []);
+  };
+  //onClick changes false selected to true, indicating whether a player has clicked the pic yet
+  const onClick = (i) => {
+    let cardsCopy = [...cards];
+    //if user clicks a card they already have clicked
+    if (cardsCopy[i].selected === true) {
+      setCards(originalCards);
+      return;
+    } else {
+      cardsCopy[i].selected = true;
+      setCards(cardsCopy);
+      randomizeCards();
+    }
+  };
 
+  const consoleLogger = () => {
+    console.log("consoleLogger");
+    console.log(cards);
+  };
+  consoleLogger();
   return (
     <>
-      {cards.map((card) => {
-        return <div>{card.name}</div>;
-      })}
+      <Score cards={cards} />
+      <br></br>
+      <div id="cardsContainer">
+        {cards.map((card, i) => {
+          return (
+            <div key={i}>
+              <button className="cardBtn" onClick={onClick.bind(this, i)}>
+                <img src={card.url} alt="pic" className="img"></img>
+                <div>{card.name}</div>
+              </button>
+              <br></br>
+              <br></br>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
---
+++ src/Components/Score.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+function Score(props) {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  useEffect(() => {
+    countScore();
+    recordBestScore();
+  });
+  const countScore = () => {
+    let selectedCards = props.cards.filter((card) => card.selected === true);
+    let selectedLength = selectedCards.length;
+
+    setCurrentScore(selectedLength);
+  };
+
+  const recordBestScore = () => {
+    if (bestScore < currentScore) {
+      setBestScore(currentScore);
+    }
+  };
+
+  return (
+    <>
+      <div>Score {currentScore}</div>
+      <div>Best Score {bestScore}</div>
+    </>
+  );
+}
+
+export default Score;
