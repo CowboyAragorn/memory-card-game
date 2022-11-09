b7d7f67c2a3bca0b2441da7bec560d9a484ca11f
CowboyAragorn <jacobman0313@gmail.com>
2022-11-09 11:13:14 -0500

    style: change colors, add instructions, adjust sizing

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,14 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Score from "./Score";
-import originalCards from "./originalCards";
 
 function Cards() {
+  //kept these in the same file so I don't have to worry about object cloning, would probably be nicer to break them out
+  const originalCards = [
+    {
+      name: "The Grapes of Wrath",
+      selected: false,
+      url: require("./../images/The_Grapes_of_Wrath_(1939_1st_ed_cover).jpg"),
+    },
+    {
+      name: "The Sound and the Fury ",
+      selected: false,
+      url: require("/home/jacob/theOdinProject/memory-card-game/src/images/The_Sound_and_the_Fury_(1929_1st_ed_dust_jacket).jpg"),
+    },
+
+    {
+      name: "The Great Gatsby",
+      selected: false,
+      url: require("/home/jacob/theOdinProject/memory-card-game/src/images/The_Great_Gatsby.jpg"),
+    },
+    {
+      name: "To Kill a Mockingbird",
+      selected: false,
+      url: require("/home/jacob/theOdinProject/memory-card-game/src/images/To_Kill_a_Mockingbird_(first_edition_cover).jpg"),
+    },
+    {
+      name: "Uncle Tom's Cabin",
+      selected: false,
+      url: require("/home/jacob/theOdinProject/memory-card-game/src/images/uncle_toms_cabin.jpg"),
+    },
+    {
+      name: "Their Eyes Were Watching God",
+      selected: false,
+      url: require("/home/jacob/theOdinProject/memory-card-game/src/images/TheirEyesWereWatchingGod.jpeg"),
+    },
+  ];
+
   //set cards state equal to imported original cards
   const [cards, setCards] = useState(originalCards);
-
-  useEffect(() => {
-    console.log("component did mount");
-  }, [cards]);
 
   //Randomize order of the cards for game
   const randomizeCards = () => {
@@ -30,6 +60,8 @@ function Cards() {
     let cardsCopy = [...cards];
     //if user clicks a card they already have clicked
     if (cardsCopy[i].selected === true) {
+      console.log("originalCards");
+      console.log(originalCards);
       setCards(originalCards);
       return;
     } else {
@@ -39,22 +71,31 @@ function Cards() {
     }
   };
 
+  //logs after the async updates to cards
   const consoleLogger = () => {
     console.log("consoleLogger");
     console.log(cards);
   };
-  consoleLogger();
+  //consoleLogger();
   return (
     <>
-      <Score cards={cards} />
+      <div id="scoreHeader">
+        <h1 id="siteTitle">Great American Memory Game</h1>
+        <div id="score">
+          <Score cards={cards} />
+        </div>
+      </div>
       <br></br>
+      <div id="instructions">
+        TO WIN: Click every book once without clicking the same book twice
+      </div>
       <div id="cardsContainer">
         {cards.map((card, i) => {
           return (
             <div key={i}>
               <button className="cardBtn" onClick={onClick.bind(this, i)}>
-                <img src={card.url} alt="pic" className="img"></img>
-                <div>{card.name}</div>
+                <img src={card.url} alt={card.name} className="img"></img>
+                <p className="cardTitle">{card.name}</p>
               </button>
               <br></br>
               <br></br>
